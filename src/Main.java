b445c14ea1ef5555e31e0d636e7af72243c91d3d
jylia91 <lievsk@gmail.com>
2022-09-19 20:09:41 +0300

    еу

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        {
+
+            for (int j = 10; j >= 1; j--) {
+                System.out.println(j);
+            }
+
+
+            for (int f = 7; f < 31; f = f + 7) {
+                System.out.println("Сегодня пятница " + f + "число. Мутим отчет)");
+            }
+
+
+            for (int ear = 1896; ear < 2121; ear = ear + 79) {
+                System.out.println("комета пролетает в " + ear + " году");
+            }
+
+        }
+    }
+}
